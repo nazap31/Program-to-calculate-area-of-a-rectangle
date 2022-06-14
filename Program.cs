a980d3faf42ef081dbd9fbc438294f47583bf0b0
nazap31 <you@example.com>
2022-06-14 03:25:13 -0600

    Program to calculate area of a rectangle

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Calcular area de un rectangulo
+
+// largo
+int largo;
+Console.WriteLine("Porfavor ingrese el largo del rectangulo");
+largo = int.Parse(Console.ReadLine());
+
+// Ancho
+int ancho;
+Console.WriteLine("Porfavor ingrese el ancho del rectangulo");
+ancho = int.Parse(Console.ReadLine());
+
+// Area
+double area; 
+area = (largo * ancho);
+Console.Write("El area del rectangulo es ");
+Console.Write(area);
